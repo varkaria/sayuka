f78ed1cbcf83a15c82d39eb78a2404b8b6f26bef
Kitpipat Jaritwong <Kitpipat.0647@gmail.com>
2021-07-23 17:21:02 +0700

    refactor code

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,11 +29,12 @@ export default {
   },
   methods: { isMobile() { 
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) 
-    { return true } 
-    else { return false }  
-    } 
-  }
-}
+      { return true } 
+    else 
+      { return false }  
+    }, 
+  },
+};
 </script>
 
 <style>
